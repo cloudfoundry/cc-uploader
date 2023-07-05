cfaa63e750aa78f401e43e536f1086b242c98ab5
M. Oleske <moleske@users.noreply.github.com>
2023-07-05 16:43:50 -0700

    Revert "Bump google.golang.org/grpc from 1.36.0 to 1.53.0 (#6)" (#7)
    This reverts commit ec303a8792c6439c9f7d69f4aee89bbaa16d6042.

--- vendor/golang.org/x/sys/unix/syscall.go
+++ vendor/golang.org/x/sys/unix/syscall.go
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris || zos
-// +build aix darwin dragonfly freebsd linux netbsd openbsd solaris zos
+// +build aix darwin dragonfly freebsd linux netbsd openbsd solaris
 
 // Package unix contains an interface to the low-level operating system
 // primitives. OS details vary depending on the underlying system, and
@@ -29,6 +28,8 @@ import (
 	"bytes"
 	"strings"
 	"unsafe"
+
+	"golang.org/x/sys/internal/unsafeheader"
 )
 
 // ByteSliceFromString returns a NUL-terminated slice of bytes
@@ -80,7 +81,13 @@ func BytePtrToString(p *byte) string {
 		ptr = unsafe.Pointer(uintptr(ptr) + 1)
 	}
 
-	return string(unsafe.Slice(p, n))
+	var s []byte
+	h := (*unsafeheader.Slice)(unsafe.Pointer(&s))
+	h.Data = unsafe.Pointer(p)
+	h.Len = n
+	h.Cap = n
+
+	return string(s)
 }
 
 // Single-word zero for use when we need a valid pointer to 0 bytes.
